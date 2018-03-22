43882fa023476d0ff8e4bee2e933f01c0196df33
Andrew Usher <andrewusher00@gmail.com>
2018-03-22 13:06:01 -0500

    Add stylus and css support

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
+import './style.styl'
 import App from './App'
 
 render(<App />, document.getElementById('root'))
 
 if (module.hot) {
-    module.hot.accept()
+  module.hot.accept()
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,38 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'docs')
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './docs',
-        compress: true,
-        hot: true,
-        port: 3000,
-        stats: 'errors-only'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            }
-        ]
-    },
-    plugins: [
-        new webpack.NamedModulesPlugin(),
-        new webpack.HotModuleReplacementPlugin()
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'docs')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './docs',
+    compress: true,
+    hot: true,
+    port: 3000,
+    stats: 'errors-only'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.styl$/,
+        use: ['style-loader', 'css-loader', 'stylus-loader']
+      },
+      {
+        test: /\.css$/,
+        use: 'css-loader'
+      }
     ]
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ]
 }
