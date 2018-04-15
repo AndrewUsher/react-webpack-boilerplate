3a724c1c8fe1e5f6c3fb9ec6379801b67c855dc1
Andrew Usher <andrewusher00@gmail.com>
2018-04-15 18:30:02 -0500

    Upgrade dependencies

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 
 class App extends Component {
-    render () {
-        return (
-            <div>
-                <h1>Hello World</h1>
-            </div>
-        )
-    }
+  render () {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
 }
 
 export default App
